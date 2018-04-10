568295e48e2d4cace924a52524809ab060be7330
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-04-10 14:16:29 +0200

    Corect

--- gestion-users.sh
+++ gestion-users.sh
@@ -754,10 +754,10 @@ if FONCYES "$VALIDE"; then
 				echo " 7 - Pour redémarrer PLEX "
 				echo " 9 - Pour installer une ancienne version(1.12) "
 				read choix
-				if choix="1"; then
+				if [[ "$choix" == 1]]; then
 					apt-get remove plexmediaserver -y
 
-				elif choix="4"; then
+				elif [[ "$choix" == 4]]; then
 					echo " Etes vous sur ? Y/N "
 					read answer
 					if answer=Y; then
