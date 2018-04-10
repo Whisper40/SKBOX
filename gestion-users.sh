d67a3d4d0d57821ca79b0416d865521c2f8cd95a
Whisper40 <30440458+Whisper40@users.noreply.github.com>
2018-04-10 14:36:15 +0200

    k

--- gestion-users.sh
+++ gestion-users.sh
@@ -767,7 +767,7 @@ if FONCYES "$VALIDE"; then
 					else
 						 break
 
-				elif ['$choix' == '7'] then
+				elif ['$choix' == '7']; then
 					service plexmediaserver stop
 					service plexmediaserver start
 
